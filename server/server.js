04cf724dc90eb615397c4c02304593171fcdc008
Stylianos Thomas <stylianos.thomas@gmail.com>
2020-11-10 16:50:38 +0000

    added a few lists and the routes to get them

--- server/server.js
+++ server/server.js
@@ -9,3 +9,15 @@ server.use(bodyParser.json());
 
 
 server.listen(port, () => console.log(`Server started in http://localhost:${port}`));
+
+music = ["Jazz","Rock","Pop","Heavy Metal","Opera"];
+movies = ["Horror","Adventure","Comedy","Sci-fi","Drama"];
+food = ["Pizza","Pasta","Burger","Tacos","Hotdogs"];
+drinks = ["Whiskey","Beer","Wine","Jin","Tequila"];
+
+server.get('/', (req, res) => res.send('Hello World!'))
+
+server.get('/music', (req, res) => res.send({music}));
+server.get('/movies', (req, res) => res.send({movies}));
+server.get('/food', (req, res) => res.send({food}));
+server.get('/drinks', (req, res) => res.send({drinks}));
