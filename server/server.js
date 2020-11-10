2ef2030c77c24a395a8c746c19c4be31c462c45a
Stylianos Thomas <stylianos.thomas@gmail.com>
2020-11-10 17:06:18 +0000

    no major changes here

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const port = 3000;
 
 const server = express();
 server.use(cors());
-server.use(bodyParser.json());
+//server.use(bodyParser.json());
 
 
 server.listen(port, () => console.log(`Server started in http://localhost:${port}`));
